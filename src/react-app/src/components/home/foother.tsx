b0e5177e5a055b907b666948d9a0aa22b71d1efd
Sergey Pugachev <pugachev@amazon.com>
2023-11-03 11:22:27 +0000

    Fix repository address

--- src/react-app/src/components/home/foother.tsx
+++ src/react-app/src/components/home/foother.tsx
@@ -6,12 +6,12 @@ export function Foother() {
           <p className="text-center text-base text-gray-400">
             View documentation on&nbsp;
             <a
-              href=" https://github.com/aws-samples/aws-cdk-builder"
+              href="https://github.com/aws-samples/aws-cdk-stack-builder-tool"
               target="_blank"
               className="text-gray-500"
               rel="noreferrer"
             >
-              github.com/aws-samples/aws-cdk-builder
+              github.com/aws-samples/aws-cdk-stack-builder-tool
             </a>
           </p>
         </div>
